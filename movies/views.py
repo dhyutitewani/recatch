0c0506c846c027105aabe21d0a6d784e3ab96f14
dhyuti <dhyuti@example.com>
2023-08-24 12:02:36 +0530

    register page

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def home(request):
     return render(request, 'movies/home.html')
+
+def register(request):
+    return redirect('register')
